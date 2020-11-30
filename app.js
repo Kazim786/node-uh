0449cc670038d7a867e8a49f1e8a51f576047d7a
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-11-29 22:22:31 -0600

    added 404

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const http = require("http");
+
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-server.listen(3000);
+const bodyParser = require("body-parser");
+
+//Need a parser middleware before anything
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes); 
+//Order doesnt matter anymore. 
+//So shopRoutes can be put before adminRoutes too. 
+//Only because its not router.use and its router.get. 
+//router.get and router.post do exact match of the url. 
+//So it wont show the '/'
+
+//even though in app.js file we do app.use for middleware. As long as the route files use .get or .post we can do any order
+
+app.use(shopRoutes);
+
+
+app.use((req, res, next) => { //catch all middleware - 404 for any invalid url
+    res.status(404).send('<h1>Page not found</h1>');
+
+});
+
+
+//Substitutes server.listen(3000)
+app.listen(3000);
 
